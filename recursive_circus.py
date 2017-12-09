e7825cb4e18ec4eea52357b692d99457bcb2d024
casey billman <>
2017-12-09 16:39:45 -0600

    end day 7

--- recursive_circus.py
+++ recursive_circus.py
@@ -27,14 +27,52 @@ class Program:
       print 'no children'
       return True
     else:
-      random_child_weight = programs[self.children[0]].getTowerWeight()
-      for child in self.children:
-        if programs[child].getTowerWeight() != random_child_weight:
+      first_child = programs[self.children[0]].getTowerWeight()
+      for child in self.children[1:len(self.children)]:
+        if programs[child].getTowerWeight() != first_child:
           return False
       return True
 
   def find_imbalance(self):
-    
+    # print
+    # print 'searching', self.name
+    # for child in self.children:
+    #   print child, programs[child].getTowerWeight(), programs[child].is_balanced()
+
+    child_count = len(self.children)
+
+    if child_count == 0:
+      print 'no children'
+      return None
+    elif child_count == 1:
+      return programs[self.children[0]].find_imbalance()
+    elif child_count > 0:
+        for child in self.children:
+          if programs[child].is_balanced() == False:
+            return programs[child].find_imbalance()
+        
+        # if above fails check if all children are equal
+        # first check if first child is equal to second child
+        # we must use three children because with only two the imbalance cannot be determined
+        first_child = programs[self.children[0]]
+        second_child = programs[self.children[1]]
+        third_child = programs[self.children[2]]
+
+        if first_child.getTowerWeight() == second_child.getTowerWeight():
+          # first child is a reliable comparison
+          for child in self.children[2:child_count]:
+            if programs[child].getTowerWeight() != first_child.getTowerWeight():
+              weight_diff = first_child.getTowerWeight() - programs[child].getTowerWeight()
+              return programs[child].weight + weight_diff
+        else:
+          if first_child.getTowerWeight() == third_child.getTowerWeight():
+            # second child is off
+            weight_diff = first_child.getTowerWeight() - second_child.getTowerWeight()
+            return second_child.weight + weight_diff
+          elif second_child.getTowerWeight() == third_child.getTowerWeight():
+            # first child is off
+            weight_diff = first_child.getTowerWeight() - second_child.getTowerWeight()
+            return first_child.weight + weight_diff
 
 programs = {}
 children = {}
@@ -70,8 +108,23 @@ for program in programs:
 print "root is", root.name
 print "weight", root.getTowerWeight()
 
-for sub_tower in root.children:
-  if 
-  print programs[sub_tower].is_balanced()
-  print programs[sub_tower].getTowerWeight()
+# for sub_tower in root.children:
+#   print programs[sub_tower].name
+#   print programs[sub_tower].is_balanced()
+#   print programs[sub_tower].getTowerWeight()
 
+imblance = root.find_imbalance()
+print imblance
+
+# for child in imblance.children:
+#   print programs[child].getTowerWeight()
+
+# get tower weight test
+# print 'pgvds', programs['pgvds'].weight, programs['pgvds'].getTowerWeight()
+'''
+lessons learned (again)
+* read the instructions
+* this is how to make a comment block in python
+* python regex
+* writing your assumptions in docs isn't that bad
+'''
